350a0958f641386a803489c42d39b7df95857909
Mangal-Devanshu <sarojmangal7600@gmail.com>
2025-01-16 16:22:21 +0530

    Added Tailwind Integration And Removed prebuilt css with vite

--- frontend/taskmanager-frontend/src/main.jsx
+++ frontend/taskmanager-frontend/src/main.jsx
@@ -2,9 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './styles/navbar.css';
-import './styles/footer.css';
-import './styles/layout.css';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
