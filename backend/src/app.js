057a7da2a9164885a98fa9602fff5ccfa02222ec
Deep Gadhiya <Deepvgadhiya@gmail.com>
2025-01-17 19:42:11 +0530

    implemented getTask and getMember routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js"
+import memberRoutes from "./routes/member.route.js"
+import taskRoutes from "./routes/task.route.js"
 import {connectDb} from "./lib/db.js"
  
 dotenv.config({path : '../.env'});
@@ -14,6 +16,8 @@ app.use(cors({
 app.use(express.json());
 
 app.use("/api/auth",authRoutes);
+app.use("/api/member",memberRoutes)
+app.use("/api/task",taskRoutes)
  
 
 
